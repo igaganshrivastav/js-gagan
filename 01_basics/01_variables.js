8a03493ab2fda309c15487e8ec14713dcf6c604d
igaganshrivastav <igaganshrivastav@gmail.com>
2024-12-28 20:58:17 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "gagan@google.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+let accountState;
+
+// accountId = 2
+
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
