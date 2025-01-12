64292b715b4c33d2f79e41ef796129fa18dd2d97
igaganshrivastav <igaganshrivastav@gmail.com>
2025-01-12 17:44:45 +0000

    data operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -20,7 +20,35 @@
 
 let someNumber = 12.22;
 
-let stringNumber = String(someNumber)
-console.log(typeof someNumber);
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(typeof someNumber);
+//console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+////////// *************** Operations ***********************
+
+// let value = 3
+// let negValue = -value
+// console.log(negValue);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2/3);
+//console.log(2%3)
+
+// let str1 = "hello"
+// let str2 = " gagan"
+
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2 + 10);
+// console.log(1 + 2 + 20 + "2" + 2 + 2);
+
+// console.log( (3+4) * 5 % 3);
+
+
