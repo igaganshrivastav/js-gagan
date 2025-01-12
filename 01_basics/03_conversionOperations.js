e9222d095bdce7cb4ed16f75125db7674cec6a48
igaganshrivastav <igaganshrivastav@gmail.com>
2025-01-12 17:05:19 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+// let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => Nan
+// true => 1; false => 0
+
+// let isLoggedIn = 10
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(typeof isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+let someNumber = 12.22;
+
+let stringNumber = String(someNumber)
+console.log(typeof someNumber);
+console.log(typeof stringNumber);
+console.log(stringNumber);
